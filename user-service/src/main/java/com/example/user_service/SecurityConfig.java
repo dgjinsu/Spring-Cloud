17bf9c396be53e251adfee393ca250fe42763adf
dgjinsu <rlawlstn3206@naver.com>
2024-07-01 16:19:36 +0900

    feat: 시큐리티 컨피그 파일 작성

---
+++ user-service/src/main/java/com/example/user_service/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.user_service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+            .formLogin(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers(new AntPathRequestMatcher("/users/**")).permitAll());
+
+        return http.build();
+    }
+
+}
