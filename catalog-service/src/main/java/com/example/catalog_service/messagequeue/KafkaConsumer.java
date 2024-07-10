a9ef10517555838499ba7f25b65f357ac207d8af
dgjinsu <rlawlstn3206@naver.com>
2024-07-10 16:14:21 +0900

    feat: Catalog 수량 감소 Consumer 로직 작성

---
+++ catalog-service/src/main/java/com/example/catalog_service/messagequeue/KafkaConsumer.java
@@ -0,0 +1,43 @@
+package com.example.catalog_service.messagequeue;
+
+import com.example.catalog_service.jpa.CatalogEntity;
+import com.example.catalog_service.jpa.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaConsumer {
+    private final CatalogRepository catalogRepository;
+
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQty(String kafkaMessage) {
+        log.info("kafka Message: ->" + kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+
+        CatalogEntity catalogEntity = catalogRepository.findByProductId((String) map.get("productId"))
+            .orElseThrow(() -> new EntityNotFoundException("Catalog Not Found"));
+
+        catalogEntity.setStock(catalogEntity.getStock() - (Integer) map.get("qty"));
+        catalogRepository.save(catalogEntity);
+    }
+
+}
