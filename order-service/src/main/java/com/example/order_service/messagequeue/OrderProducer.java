ec9bd25efa6a30698c3e45e04d13c8f5ec82b4df
dgjinsu <rlawlstn3206@naver.com>
2024-07-15 00:40:40 +0900

    fix: 오탈자 수정 (init -> int)

--- order-service/src/main/java/com/example/order_service/messagequeue/OrderProducer.java
+++ order-service/src/main/java/com/example/order_service/messagequeue/OrderProducer.java
@@ -24,9 +24,9 @@ public class OrderProducer {
         new Field("string", true, "order_id"),
         new Field("string", true, "user_id"),
         new Field("string", true, "product_id"),
-        new Field("init32", true, "qty"),
-        new Field("init32", true, "unit_price"),
-        new Field("init32", true, "total_price")
+        new Field("int32", true, "qty"),
+        new Field("int32", true, "unit_price"),
+        new Field("int32", true, "total_price")
         );
 
     Schema schema = Schema.builder()
